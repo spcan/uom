080411386c18b3779fb1bb0ee79a3b513392eaa9
Mike Boutin <mike.boutin@gmail.com>
2019-08-16 13:20:25 -0400

    [ci skip] f si

--- src/si/angle.rs
+++ src/si/angle.rs
@@ -2,7 +2,8 @@
 
 uom_macros::quantity! {
     /// Angle (dimensionless quantity).
-    quantity: Angle; "angle";
+    quantity: Angle;
+    description: "angle";
     /// Dimension of angle, 1 (dimensionless).
     dimension: ISQ<
         Z0,     // length
@@ -16,13 +17,13 @@ uom_macros::quantity! {
     units {
         /// SI derived unit of angle. It is the angle subtended at the center of a circle by an
         /// arc that is equal in length to the radius of the circle.
-        @radian: 1.0_E0; "rad", "radian", "radians";
-        @revolution: 6.283_185_307_179_586_E0; "r", "revolution", "revolutions";
-        @degree: 1.745_329_251_994_329_5_E-2; "°", "degree", "degrees";
-        @gon: 1.570_796_326_794_896_7_E-2; "gon", "gon", "gons";
-        @mil: 9.817_477_E-4; "mil", "mil", "mils";
-        @minute: 2.908_882_086_657_216_E-4; "′", "minute", "minutes";
-        @second: 4.848_136_811_095_36_E-6; "″", "second", "seconds";
+        radian: 1.0_E0; "rad", "radian", "radians";
+        revolution: 6.283_185_307_179_586_E0; "r", "revolution", "revolutions";
+        degree: 1.745_329_251_994_329_5_E-2; "°", "degree", "degrees";
+        gon: 1.570_796_326_794_896_7_E-2; "gon", "gon", "gons";
+        mil: 9.817_477_E-4; "mil", "mil", "mils";
+        minute: 2.908_882_086_657_216_E-4; "′", "minute", "minutes";
+        second: 4.848_136_811_095_36_E-6; "″", "second", "seconds";
     }
 }
 
--- src/si/mod.rs
+++ src/si/mod.rs
@@ -4,9 +4,6 @@
 //! [si]: http://jcgm.bipm.org/vim/en/1.16.html
 //! [isq]: http://jcgm.bipm.org/vim/en/1.6.html
 
-// #[macro_use]
-// mod prefix;
-
 uom_macros::system! {
     /// [International System of Quantities](http://jcgm.bipm.org/vim/en/1.6.html) (ISQ).
     name: ISQ;
@@ -47,242 +44,229 @@ uom_macros::system! {
     }
     quantities {
         mod acceleration::Acceleration;
-        mod amount_of_substance::AmountOfSubstance;
-        mod angle::Angle;
-        mod angular_acceleration::AngularAcceleration;
-        mod angular_jerk::AngularJerk;
-        mod angular_velocity::AngularVelocity;
-        mod area::Area;
-        mod available_energy::AvailableEnergy;
-        mod capacitance::Capacitance;
-        mod electric_charge::ElectricCharge;
-        mod electric_current::ElectricCurrent;
-        mod electric_potential::ElectricPotential;
-        mod electrical_conductance::ElectricalConductance;
-        mod electrical_resistance::ElectricalResistance;
-        mod energy::Energy;
-        mod force::Force;
-        mod frequency::Frequency;
-        mod inductance::Inductance;
-        mod information::Information;
-        mod information_rate::InformationRate;
-        mod jerk::Jerk;
-        mod length::Length;
-        mod luminance::Luminance;
-        mod luminous_intensity::LuminousIntensity;
-        mod magnetic_flux::MagneticFlux;
-        mod magnetic_flux_density::MagneticFluxDensity;
-        mod mass::Mass;
-        mod mass_density::MassDensity;
-        mod mass_rate::MassRate;
-        mod momentum::Momentum;
-        mod power::Power;
-        mod pressure::Pressure;
-        mod ratio::Ratio;
-        mod temperature_interval::TemperatureInterval;
-        mod thermodynamic_temperature::ThermodynamicTemperature;
-        mod time::Time;
-        mod torque::Torque;
-        mod velocity::Velocity;
-        mod volume::Volume;
-        mod volume_rate::VolumeRate;
+        // mod amount_of_substance::AmountOfSubstance;
+        // mod angle::Angle;
+        // mod angular_acceleration::AngularAcceleration;
+        // mod angular_jerk::AngularJerk;
+        // mod angular_velocity::AngularVelocity;
+        // mod area::Area;
+        // mod available_energy::AvailableEnergy;
+        // mod capacitance::Capacitance;
+        // mod electric_charge::ElectricCharge;
+        // mod electric_current::ElectricCurrent;
+        // mod electric_potential::ElectricPotential;
+        // mod electrical_conductance::ElectricalConductance;
+        // mod electrical_resistance::ElectricalResistance;
+        // mod energy::Energy;
+        // mod force::Force;
+        // mod frequency::Frequency;
+        // mod inductance::Inductance;
+        // mod information::Information;
+        // mod information_rate::InformationRate;
+        // mod jerk::Jerk;
+        // mod length::Length;
+        // mod luminance::Luminance;
+        // mod luminous_intensity::LuminousIntensity;
+        // mod magnetic_flux::MagneticFlux;
+        // mod magnetic_flux_density::MagneticFluxDensity;
+        // mod mass::Mass;
+        // mod mass_density::MassDensity;
+        // mod mass_rate::MassRate;
+        // mod momentum::Momentum;
+        // mod power::Power;
+        // mod pressure::Pressure;
+        // mod ratio::Ratio;
+        // mod temperature_interval::TemperatureInterval;
+        // mod thermodynamic_temperature::ThermodynamicTemperature;
+        // mod time::Time;
+        // mod torque::Torque;
+        // mod velocity::Velocity;
+        // mod volume::Volume;
+        // mod volume_rate::VolumeRate;
     }
 }
 
-/// [`Quantity`](struct.Quantity.html) type aliases using the default base units and parameterized
-/// on the underlying storage type.
-pub mod quantities {
-    ISQ!(si);
-}
+///// Primitive traits and types representing basic properties of types specific to the SI.
+//pub mod marker {
+//    use si::{Dimension, Quantity, Units};
+//    use Kind;
 
-storage_types! {
-    /// [`Quantity`](struct.Quantity.html) type aliases using the default base units.
-    pub types: All;
+//    /// AngleKind is a `Kind` for separating angular quantities from their identically dimensioned
+//    /// non-angular quantity counterparts. Conversions to and from `AngleKind` quantities are
+//    /// supported through implementations of the `From` trait.
+//    ///
+//    #[cfg_attr(feature = "f32", doc = " ```rust")]
+//    #[cfg_attr(not(feature = "f32"), doc = " ```rust,ignore")]
+//    /// # use uom::si::f32::*;
+//    /// # use uom::si::angle::radian;
+//    /// let a: Angle = Angle::new::<radian>(1.0);
+//    /// let r: Ratio = a.into();
+//    /// ```
+//    pub trait AngleKind: ::Kind {}
 
-    ISQ!(si, V);
-}
+//    /// InformationKind is a `Kind` for separating information quantities from their identically
+//    /// dimensioned non-information quantity counterparts. Conversions to and from
+//    /// `InformationKind` quantities are supported through implementations of the `From` trait.
+//    ///
+//    #[cfg_attr(feature = "f32", doc = " ```rust")]
+//    #[cfg_attr(not(feature = "f32"), doc = " ```rust,ignore")]
+//    /// # use uom::si::f32::*;
+//    /// # use uom::si::information::kilobyte;
+//    /// let i: Information = Information::new::<kilobyte>(1.0);
+//    /// let r: Ratio = i.into();
+//    /// ```
+//    pub trait InformationKind: ::Kind {}
 
-/// Primitive traits and types representing basic properties of types specific to the SI.
-pub mod marker {
-    use si::{Dimension, Quantity, Units};
-    use Kind;
+//    /// Kind of thermodynamic temperature.
+//    pub trait TemperatureKind:
+//        ::marker::Mul
+//        + ::marker::MulAssign
+//        + ::marker::Div
+//        + ::marker::DivAssign
+//        + ::marker::Rem
+//        + ::marker::RemAssign
+//    {
+//    }
 
-    /// AngleKind is a `Kind` for separating angular quantities from their identically dimensioned
-    /// non-angular quantity counterparts. Conversions to and from `AngleKind` quantities are
-    /// supported through implementations of the `From` trait.
-    ///
-    #[cfg_attr(feature = "f32", doc = " ```rust")]
-    #[cfg_attr(not(feature = "f32"), doc = " ```rust,ignore")]
-    /// # use uom::si::f32::*;
-    /// # use uom::si::angle::radian;
-    /// let a: Angle = Angle::new::<radian>(1.0);
-    /// let r: Ratio = a.into();
-    /// ```
-    pub trait AngleKind: ::Kind {}
+//    /// `impl_from` generates generic inter-Kind implementations of `From`.
+//    #[cfg(feature = "autoconvert")]
+//    #[macro_export]
+//    macro_rules! impl_from {
+//        ($a:ident, $b:ident) => {
+//            impl<L, M, T, I, Th, N, J, Ul, Ur, V>
+//                From<
+//                    Quantity<
+//                        dyn Dimension<
+//                            L = L,
+//                            M = M,
+//                            T = T,
+//                            I = I,
+//                            Th = Th,
+//                            N = N,
+//                            J = J,
+//                            Kind = dyn $a,
+//                        >,
+//                        Ur,
+//                        V,
+//                    >,
+//                >
+//                for Quantity<
+//                    dyn Dimension<L = L, M = M, T = T, I = I, Th = Th, N = N, J = J, Kind = dyn $b>,
+//                    Ul,
+//                    V,
+//                >
+//            where
+//                Ul: Units<V> + ?Sized,
+//                Ur: Units<V> + ?Sized,
+//                V: ::num_traits::Num + ::Conversion<V>,
+//            {
+//                fn from(
+//                    val: Quantity<
+//                        dyn Dimension<
+//                            L = L,
+//                            M = M,
+//                            T = T,
+//                            I = I,
+//                            Th = Th,
+//                            N = N,
+//                            J = J,
+//                            Kind = dyn $a,
+//                        >,
+//                        Ur,
+//                        V,
+//                    >,
+//                ) -> Quantity<
+//                    dyn Dimension<L = L, M = M, T = T, I = I, Th = Th, N = N, J = J, Kind = dyn $b>,
+//                    Ul,
+//                    V,
+//                > {
+//                    Self {
+//                        dimension: ::lib::marker::PhantomData,
+//                        units: ::lib::marker::PhantomData,
+//                        value: super::change_base::<
+//                            dyn Dimension<
+//                                L = L,
+//                                M = M,
+//                                T = T,
+//                                I = I,
+//                                Th = Th,
+//                                N = N,
+//                                J = J,
+//                                Kind = dyn $b,
+//                            >,
+//                            Ul,
+//                            Ur,
+//                            V,
+//                        >(&val.value),
+//                    }
+//                }
+//            }
+//        };
+//    }
 
-    /// InformationKind is a `Kind` for separating information quantities from their identically
-    /// dimensioned non-information quantity counterparts. Conversions to and from
-    /// `InformationKind` quantities are supported through implementations of the `From` trait.
-    ///
-    #[cfg_attr(feature = "f32", doc = " ```rust")]
-    #[cfg_attr(not(feature = "f32"), doc = " ```rust,ignore")]
-    /// # use uom::si::f32::*;
-    /// # use uom::si::information::kilobyte;
-    /// let i: Information = Information::new::<kilobyte>(1.0);
-    /// let r: Ratio = i.into();
-    /// ```
-    pub trait InformationKind: ::Kind {}
+//    /// `impl_from` generates generic inter-Kind implementations of `From`.
+//    #[cfg(not(feature = "autoconvert"))]
+//    #[macro_export]
+//    macro_rules! impl_from {
+//        ($a:ident, $b:ident) => {
+//            impl<L, M, T, I, Th, N, J, U, V>
+//                From<
+//                    Quantity<
+//                        dyn Dimension<
+//                            L = L,
+//                            M = M,
+//                            T = T,
+//                            I = I,
+//                            Th = Th,
+//                            N = N,
+//                            J = J,
+//                            Kind = dyn $a,
+//                        >,
+//                        U,
+//                        V,
+//                    >,
+//                >
+//                for Quantity<
+//                    dyn Dimension<L = L, M = M, T = T, I = I, Th = Th, N = N, J = J, Kind = dyn $b>,
+//                    U,
+//                    V,
+//                >
+//            where
+//                U: Units<V> + ?Sized,
+//                V: ::num_traits::Num + ::Conversion<V>,
+//            {
+//                fn from(
+//                    val: Quantity<
+//                        dyn Dimension<
+//                            L = L,
+//                            M = M,
+//                            T = T,
+//                            I = I,
+//                            Th = Th,
+//                            N = N,
+//                            J = J,
+//                            Kind = dyn $a,
+//                        >,
+//                        U,
+//                        V,
+//                    >,
+//                ) -> Quantity<
+//                    dyn Dimension<L = L, M = M, T = T, I = I, Th = Th, N = N, J = J, Kind = dyn $b>,
+//                    U,
+//                    V,
+//                > {
+//                    Self {
+//                        dimension: ::lib::marker::PhantomData,
+//                        units: ::lib::marker::PhantomData,
+//                        value: val.value,
+//                    }
+//                }
+//            }
+//        };
+//    }
 
-    /// Kind of thermodynamic temperature.
-    pub trait TemperatureKind:
-        ::marker::Mul
-        + ::marker::MulAssign
-        + ::marker::Div
-        + ::marker::DivAssign
-        + ::marker::Rem
-        + ::marker::RemAssign
-    {
-    }
-
-    /// `impl_from` generates generic inter-Kind implementations of `From`.
-    #[cfg(feature = "autoconvert")]
-    #[macro_export]
-    macro_rules! impl_from {
-        ($a:ident, $b:ident) => {
-            impl<L, M, T, I, Th, N, J, Ul, Ur, V>
-                From<
-                    Quantity<
-                        dyn Dimension<
-                            L = L,
-                            M = M,
-                            T = T,
-                            I = I,
-                            Th = Th,
-                            N = N,
-                            J = J,
-                            Kind = dyn $a,
-                        >,
-                        Ur,
-                        V,
-                    >,
-                >
-                for Quantity<
-                    dyn Dimension<L = L, M = M, T = T, I = I, Th = Th, N = N, J = J, Kind = dyn $b>,
-                    Ul,
-                    V,
-                >
-            where
-                Ul: Units<V> + ?Sized,
-                Ur: Units<V> + ?Sized,
-                V: ::num_traits::Num + ::Conversion<V>,
-            {
-                fn from(
-                    val: Quantity<
-                        dyn Dimension<
-                            L = L,
-                            M = M,
-                            T = T,
-                            I = I,
-                            Th = Th,
-                            N = N,
-                            J = J,
-                            Kind = dyn $a,
-                        >,
-                        Ur,
-                        V,
-                    >,
-                ) -> Quantity<
-                    dyn Dimension<L = L, M = M, T = T, I = I, Th = Th, N = N, J = J, Kind = dyn $b>,
-                    Ul,
-                    V,
-                > {
-                    Self {
-                        dimension: ::lib::marker::PhantomData,
-                        units: ::lib::marker::PhantomData,
-                        value: super::change_base::<
-                            dyn Dimension<
-                                L = L,
-                                M = M,
-                                T = T,
-                                I = I,
-                                Th = Th,
-                                N = N,
-                                J = J,
-                                Kind = dyn $b,
-                            >,
-                            Ul,
-                            Ur,
-                            V,
-                        >(&val.value),
-                    }
-                }
-            }
-        };
-    }
-
-    /// `impl_from` generates generic inter-Kind implementations of `From`.
-    #[cfg(not(feature = "autoconvert"))]
-    #[macro_export]
-    macro_rules! impl_from {
-        ($a:ident, $b:ident) => {
-            impl<L, M, T, I, Th, N, J, U, V>
-                From<
-                    Quantity<
-                        dyn Dimension<
-                            L = L,
-                            M = M,
-                            T = T,
-                            I = I,
-                            Th = Th,
-                            N = N,
-                            J = J,
-                            Kind = dyn $a,
-                        >,
-                        U,
-                        V,
-                    >,
-                >
-                for Quantity<
-                    dyn Dimension<L = L, M = M, T = T, I = I, Th = Th, N = N, J = J, Kind = dyn $b>,
-                    U,
-                    V,
-                >
-            where
-                U: Units<V> + ?Sized,
-                V: ::num_traits::Num + ::Conversion<V>,
-            {
-                fn from(
-                    val: Quantity<
-                        dyn Dimension<
-                            L = L,
-                            M = M,
-                            T = T,
-                            I = I,
-                            Th = Th,
-                            N = N,
-                            J = J,
-                            Kind = dyn $a,
-                        >,
-                        U,
-                        V,
-                    >,
-                ) -> Quantity<
-                    dyn Dimension<L = L, M = M, T = T, I = I, Th = Th, N = N, J = J, Kind = dyn $b>,
-                    U,
-                    V,
-                > {
-                    Self {
-                        dimension: ::lib::marker::PhantomData,
-                        units: ::lib::marker::PhantomData,
-                        value: val.value,
-                    }
-                }
-            }
-        };
-    }
-
-    impl_from!(AngleKind, Kind);
-    impl_from!(Kind, AngleKind);
-    impl_from!(InformationKind, Kind);
-    impl_from!(Kind, InformationKind);
-}
+//    impl_from!(AngleKind, Kind);
+//    impl_from!(Kind, AngleKind);
+//    impl_from!(InformationKind, Kind);
+//    impl_from!(Kind, InformationKind);
+//}
